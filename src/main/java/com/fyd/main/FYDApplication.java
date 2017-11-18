3c3bc6e0506fec67bf90d7114892262a5df3474b
vaishsh <vaishali.shah@copart.com>
2017-11-18 16:44:47 -0600

    first commit

---
+++ src/main/java/com/fyd/main/FYDApplication.java
@@ -0,0 +1,12 @@
+package com.fyd.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FYDApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(FYDApplication.class, args);
+	}
+}
